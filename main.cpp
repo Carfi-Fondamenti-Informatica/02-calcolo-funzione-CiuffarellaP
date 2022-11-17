8eadce3f4e60d98a1e600a5065d9b63d48b7b1e9
CiuffarellaP <117938895+CiuffarellaP@users.noreply.github.com>
2022-11-17 13:31:00 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
-
 int main() {
+   for(int i=10;i>-1;i--){
+       cout<<i<<endl;
+   }
+   int j=10;
+   while (j>-1){
+       cout<<j<<endl;
+       j--;
+   }
    return 0;
 }
